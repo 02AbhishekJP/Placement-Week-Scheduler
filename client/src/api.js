7c216662ac229bb5ef2f009a8f5fd2e3ae0452d9
Abhishek JP <abhishekjp02@gmail.com>
2026-08-28 00:11:00 +0530

    Add Render deployment config and dynamic API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3001/api';
+const API = import.meta.env.VITE_API_URL || '/api';
 
 async function request(url, options = {}) {
   const res = await fetch(`${API}${url}`, {
